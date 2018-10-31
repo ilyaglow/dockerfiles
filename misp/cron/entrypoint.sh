e742b54a06904868e15419cca479bda155b95697
Ilya Glotov <contact@ilyaglotov.com>
2018-10-31 14:26:07 +0300

    Add cron container to fetch feeds

---
+++ misp/cron/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+FETCH_ROUTE=/feeds/fetchFromAllFeeds
+DEFAULT_MISP_URL=http://misp
+# Update once an hour
+DEFAULT_CRON_TIME="* 1 * * *"
+
+if [ ! "$MISP_URL" ]; then
+    MISP_URL=$DEFAULT_MISP_URL
+fi
+
+DEFAULT_CRON_CMD="curl -H 'Authorization: $MISP_AUTH' -H 'Accept: application/json' -H 'Content-Type: application/json' ${MISP_URL}${FETCH_ROUTE}"
+
+if [ ! "$CRON_TIME" ]; then
+    CRON_TIME=$DEFAULT_CRON_TIME
+fi
+
+if [ ! "$CRON_CMD" ]; then
+    CRON_CMD=$DEFAULT_CRON_CMD
+fi
+
+crontab -d \
+  && echo "${CRON_TIME} ${CRON_CMD}" | crontab - \
+  && crond -f -L /dev/stdout -l 1
