a816d001027b0d249d465e8774d4c05ec539665b
Ilya Glotov <contact@ilyaglotov.com>
2018-10-31 15:28:10 +0300

    Fix cron time

--- misp/cron/entrypoint.sh
+++ misp/cron/entrypoint.sh
@@ -2,7 +2,7 @@
 FETCH_ROUTE=/feeds/fetchFromAllFeeds
 DEFAULT_MISP_URL=http://misp
 # Update once an hour
-DEFAULT_CRON_TIME="* 1 * * *"
+DEFAULT_CRON_TIME="* */1 * * *"
 
 if [ ! "$MISP_URL" ]; then
     MISP_URL=$DEFAULT_MISP_URL
