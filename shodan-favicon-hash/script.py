486feaf3615769e8db88bcea3198b523b6047c32
Ilya Glotov <contact@ilyaglotov.com>
2020-01-12 00:43:22 +0300

    Add count if an API_KEY env var is defined

--- shodan-favicon-hash/script.py
+++ shodan-favicon-hash/script.py
@@ -1,9 +1,12 @@
-import pymmh3 as mmh3
-import requests
 import base64
 import sys
+import os
 
-url = "{}/favicon.ico".format(sys.argv[1])
+import shodan
+import pymmh3 as mmh3
+import requests
+
+url = f'{sys.argv[1]}/favicon.ico'
 
 try:
     content = requests.get(url).content
@@ -11,4 +14,11 @@ except Exception as e:
     print(e.message)
     os.exit(1)
 
-print(mmh3.hash(base64.encodebytes(content)))
+h = mmh3.hash(base64.encodebytes(content))
+print(f'http.favicon.hash:{h}')
+
+api_key = os.getenv('API_KEY', '')
+if api_key != '':
+    api = shodan.Shodan(api_key)
+    count = api.count(f'http.favicon.hash:{h}')
+    print(f'count: {count.get("total", "not available")}')
