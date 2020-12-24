aece8977713fcca81835876b549333aa13b6db14
Ilya Glotov <contact@ilyaglotov.com>
2020-12-24 16:05:26 +0300

    Add option to specify user and password

--- socksproxy/main.go
+++ socksproxy/main.go
@@ -3,15 +3,24 @@ package main
 import (
 	"flag"
 	"log"
+	"os"
 
 	"github.com/armon/go-socks5"
 )
 
 func main() {
+	conf := &socks5.Config{
+		AuthMethods: []socks5.Authenticator{
+			socks5.UserPassAuthenticator{
+				Credentials: socks5.StaticCredentials{
+					os.Getenv("USER"): os.Getenv("PASS"),
+				},
+			},
+		},
+		Logger: log.New(os.Stdout, "", log.LstdFlags),
+	}
 	port := flag.String("p", ":8080", "port")
 	flag.Parse()
-
-	conf := &socks5.Config{}
 
 	srv, err := socks5.New(conf)
 	if err != nil {
